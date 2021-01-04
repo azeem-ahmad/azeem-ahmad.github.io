b8fb12c1b9f0e0d25244d367515558a41075c341
Azeem Ahmad <41817419+azeem-ahmad@users.noreply.github.com>
2021-01-04 23:05:30 +0530

    all files uploaded

---
+++ precache-manifest.f49cc49ae584434c6d1c6020dfc87085.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b809fbbafe6c5993e28bf304c9fa9e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "192e55669cc38e93cd94",
+    "url": "/static/css/main.50b89eb1.chunk.css"
+  },
+  {
+    "revision": "2b321fc41a2b52f9acee",
+    "url": "/static/js/2.a1fe6cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1fe6cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192e55669cc38e93cd94",
+    "url": "/static/js/main.9c99aaef.chunk.js"
+  },
+  {
+    "revision": "bef689ffdd4347b4c7de",
+    "url": "/static/js/runtime-main.1afb9ad3.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "e1722994103132ab979e295840f6449d",
+    "url": "/static/media/a1-hi-tech.e1722994.jpg"
+  },
+  {
+    "revision": "560e25e5b8aa322301ba9cee2ded8cfd",
+    "url": "/static/media/about-me-bg-1.560e25e5.jpg"
+  },
+  {
+    "revision": "bc624693d8645eaf666d8d16692bfbdd",
+    "url": "/static/media/affixcenter.bc624693.jpg"
+  },
+  {
+    "revision": "c62af9fc85d7cacc31f439d48dbb194c",
+    "url": "/static/media/ahika.c62af9fc.jpg"
+  },
+  {
+    "revision": "e5976d8a12a7a71724f784d4750c61a9",
+    "url": "/static/media/align-studio.e5976d8a.jpg"
+  },
+  {
+    "revision": "dd1db23b0be9c4642c0fae4f9e4bb5a3",
+    "url": "/static/media/arrows-dark.dd1db23b.png"
+  },
+  {
+    "revision": "69a351ae345ec933f87a6ddab3c1bc9e",
+    "url": "/static/media/arrows.69a351ae.png"
+  },
+  {
+    "revision": "5eedd307244a6a5d4832c24e3af46cad",
+    "url": "/static/media/aspire-pmt.5eedd307.jpg"
+  },
+  {
+    "revision": "aec73ab9015e3e1549865605524e1334",
+    "url": "/static/media/azeem-portfolio-new.aec73ab9.jpg"
+  },
+  {
+    "revision": "2f64f3a804ec4825881a70ebb5b33c14",
+    "url": "/static/media/azeem-portfolio.2f64f3a8.jpg"
+  },
+  {
+    "revision": "bd89ebb5af713ad8800a6c87c8e72bbe",
+    "url": "/static/media/azeem.bd89ebb5.jpg"
+  },
+  {
+    "revision": "eafb446faf88a536bc13c04f8d3264fb",
+    "url": "/static/media/bigbird.eafb446f.jpg"
+  },
+  {
+    "revision": "8df99badfb3206bd2cadb8896b31fa54",
+    "url": "/static/media/byohbaraat.8df99bad.jpg"
+  },
+  {
+    "revision": "a8c874b93b3d848f39a71260c57e3863",
+    "url": "/static/media/closedhand.a8c874b9.cur"
+  },
+  {
+    "revision": "7e0c760db4a4979cb276c09ebb640d5a",
+    "url": "/static/media/contracco.7e0c760d.jpg"
+  },
+  {
+    "revision": "eec002c24523d8ed8904d3bc667e88d4",
+    "url": "/static/media/curved-border.eec002c2.svg"
+  },
+  {
+    "revision": "5498a2049d91f4b4669096731ecdddd4",
+    "url": "/static/media/eacme.5498a204.jpg"
+  },
+  {
+    "revision": "e08569c6bb411503e0bde3711013804b",
+    "url": "/static/media/educamedics.e08569c6.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "e8277d4b0e4fa234b797590859af8506",
+    "url": "/static/media/flags.e8277d4b.png"
+  },
+  {
+    "revision": "39349dc360397c1bb423be4e0308f832",
+    "url": "/static/media/gems-qmsmeds.39349dc3.jpg"
+  },
+  {
+    "revision": "ced112d253c78b2963e807852c3f7550",
+    "url": "/static/media/icons.ced112d2.png"
+  },
+  {
+    "revision": "68f1a9f3aaf32a6422a85cc834695dea",
+    "url": "/static/media/immense-hospitality.68f1a9f3.jpg"
+  },
+  {
+    "revision": "3c1e558a534cc496b248524dabe352e5",
+    "url": "/static/media/intellimed.3c1e558a.jpg"
+  },
+  {
+    "revision": "d9f464405c21f6df51d6ea4a8ec3587a",
+    "url": "/static/media/inventors-tech.d9f46440.jpg"
+  },
+  {
+    "revision": "b942834aa3f4b3e73cb8aa803a178152",
+    "url": "/static/media/inventorstech-live-training.b942834a.jpg"
+  },
+  {
+    "revision": "4d0498ffcab966b36398872a895d1745",
+    "url": "/static/media/knav.4d0498ff.jpg"
+  },
+  {
+    "revision": "f3edce593f2ba7e85f9a2d03e7fe5131",
+    "url": "/static/media/legacom.f3edce59.jpg"
+  },
+  {
+    "revision": "97361c5d6912f2e46f1b7df1f757e60e",
+    "url": "/static/media/map.97361c5d.png"
+  },
+  {
+    "revision": "187b9d8e52b3f75e495a897162602ca9",
+    "url": "/static/media/mex.187b9d8e.jpg"
+  },
+  {
+    "revision": "2917731d8d78eeb7be91e3623d40e0cd",
+    "url": "/static/media/morgan.2917731d.jpg"
+  },
+  {
+    "revision": "a570b1afcc03f0f80b29e43a22c1d280",
+    "url": "/static/media/msobcfdc.a570b1af.jpg"
+  },
+  {
+    "revision": "bfe36c4b63672472861e363fdb1ba357",
+    "url": "/static/media/naarad.bfe36c4b.jpg"
+  },
+  {
+    "revision": "a26783992bc7be7fa374c7165566ff87",
+    "url": "/static/media/nashik-cambridge-school.a2678399.jpg"
+  },
+  {
+    "revision": "2cc0c2619711cdc77df924463e644f19",
+    "url": "/static/media/online-meal-box.2cc0c261.jpg"
+  },
+  {
+    "revision": "b06c243f534d9c5461d16528156cd5a8",
+    "url": "/static/media/openhand.b06c243f.cur"
+  },
+  {
+    "revision": "cbef1e80f52eeee87a1e5dcbe7732225",
+    "url": "/static/media/pci.cbef1e80.jpg"
+  },
+  {
+    "revision": "08d2ce156a31c29aa3ec0ce6379b3199",
+    "url": "/static/media/pencil-blue-line.08d2ce15.png"
+  },
+  {
+    "revision": "66af1fa07f9f03d19f076d628921fc25",
+    "url": "/static/media/purpose-trade.66af1fa0.jpg"
+  },
+  {
+    "revision": "072a0a6b6905a552163e8a451f2c8f39",
+    "url": "/static/media/qmsmas.072a0a6b.jpg"
+  },
+  {
+    "revision": "23dd0457ac5921f359e1fdb896d7942c",
+    "url": "/static/media/qmsmeds.23dd0457.jpg"
+  },
+  {
+    "revision": "a40d241bcc7a680f8e561b90b22f6482",
+    "url": "/static/media/quattro-italia.a40d241b.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "1142a254da20b59e7d10ac39e78c4fbf",
+    "url": "/static/media/revicons.1142a254.svg"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "27d59fc881d0f50f56455adb988a8993",
+    "url": "/static/media/russet.27d59fc8.jpg"
+  },
+  {
+    "revision": "c635d4bdb9c780dae1c245e3fef29652",
+    "url": "/static/media/shoesetc.c635d4bd.jpg"
+  },
+  {
+    "revision": "d8da5c79d07d1b248941c446762394e7",
+    "url": "/static/media/south-stormont.d8da5c79.jpg"
+  },
+  {
+    "revision": "09e243ad4fd79cd054906397c6864948",
+    "url": "/static/media/ssmart-shopy.09e243ad.jpg"
+  },
+  {
+    "revision": "335cbf607c55aa32fd06809d1f9eb127",
+    "url": "/static/media/star.335cbf60.woff"
+  },
+  {
+    "revision": "5aba6f67f5a8e7baf9bc5a569824e2ba",
+    "url": "/static/media/star.5aba6f67.eot"
+  },
+  {
+    "revision": "6b5f4503d1fab4be8543d7a5bbefd0df",
+    "url": "/static/media/star.6b5f4503.svg"
+  },
+  {
+    "revision": "6b78a34cea321205e5ee8a1b44b41a5e",
+    "url": "/static/media/star.6b78a34c.ttf"
+  },
+  {
+    "revision": "09324a996df5ec2c078ca3e442e6312c",
+    "url": "/static/media/startup-freedom.09324a99.jpg"
+  },
+  {
+    "revision": "d2871ce218898d86573a0f1339f93cd2",
+    "url": "/static/media/upskill-master-landing-page.d2871ce2.jpg"
+  },
+  {
+    "revision": "bef1cf05ef586e3314a56a6c76d73499",
+    "url": "/static/media/upskill-master.bef1cf05.jpg"
+  },
+  {
+    "revision": "0c27f014e949b80e8fbfb9ade92d6e27",
+    "url": "/static/media/wallstreet.0c27f014.jpg"
+  },
+  {
+    "revision": "fce21585c127973a84640f2eb4e6ec1e",
+    "url": "/static/media/xcitedata.fce21585.jpg"
+  }
+]);
